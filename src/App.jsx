170fa081a7ffe6799dd22f44571f8aac71d9a909
SnehalRay <snehal1@ualberta.ca>
2024-06-16 23:28:26 +0400

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Button } from "@chakra-ui/react"
+import { Container } from "@chakra-ui/react"
+import { Routes } from "react-router-dom"
+import { UserPage } from "./pages/UserPage"
 
 function App() {
 
   return (
-    <>
-      <Button> Heyy</Button>
-    </>
+    <Container maxW='620px' bg='gray.900'>
+      <Routes>
+      <Route path="/:username" element={<UserPage/>}/>
+      </Routes>
+    </Container>
   )
 }
 
