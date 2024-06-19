03348598efac6b874f9d5c72a7a9a6b695d6c353
SnehalRay <snehal1@ualberta.ca>
2024-06-20 02:00:34 +0400

    JWT authorization done with cookies

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,32 +1,31 @@
-import express from 'express';
-import bcrypt, { genSalt } from 'bcrypt';
-import User from '../models/userModel.js'
+import bcrypt from 'bcrypt';
+import User from '../models/userModel.js';
+import generateToken from '../helper/generateToken.js';
 
-export const signupUser = async (req,res)=> {
-    try{
+export const signupUser = async (req, res) => {
+  try {
+    const { name, email, username, password } = req.body;
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
 
-        const {name, email, username, password} = req.body;
-        const existingUser = await User.findOne({$or:[{email},{username}]});
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
 
-        if (existingUser){
-            return res.status(500).json({error:'User already exists'})
-        }
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
 
-        //if user does not exist, we add it
+    // Create new user
+    const newUser = new User({ name, email, username, password: hashPassword });
+    await newUser.save();
 
-        //hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(password, salt);
+    if (newUser) {
+      generateToken({ id: newUser._id, username: newUser.username, email: newUser.email }, res);
+      return res.status(201).json({ message: 'User successfully created', user: newUser });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-        const newUser = new User({name, email, username, password: hashPassword});
-        await newUser.save()
-
-        res.status(201).json({message:'User successfully created: ',user:newUser});
-
-   }
-   catch (error){
-    res.status(500).json({message: error.message})
-   }
-}
-
-export default {signupUser};
+export default { signupUser };
