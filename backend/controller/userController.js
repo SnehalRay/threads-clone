ca41218583cbf262424ab0cc220ed951d07082df
SnehalRay <snehal1@ualberta.ca>
2024-06-20 02:26:29 +0400

    logging in and sign out is done

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,10 @@
 import bcrypt from 'bcrypt';
 import User from '../models/userModel.js';
 import generateToken from '../helper/generateToken.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+
 
 export const signupUser = async (req, res) => {
   try {
@@ -28,4 +32,47 @@ export const signupUser = async (req, res) => {
   }
 };
 
-export default { signupUser };
+//loggingUser
+
+export const loggingUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+
+    if (!existingUser) {
+      return res.status(401).json({ message: "User does not exist" });
+    }
+
+    // Verify password
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Password incorrect" });
+    }
+
+    // Generate token
+    generateToken({ id: existingUser._id, username: existingUser.username, email: existingUser.email }, res);
+    return res.status(200).json({ message: 'User successfully logged in', user: existingUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Signing Out
+
+export const signingOut = async (req, res) => {
+    try {
+      // Clear the JWT cookie
+      res.cookie("jwt", "", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'Strict',
+        maxAge: 0
+      });
+  
+      res.status(200).json({ message: 'User signed out successfully' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+export default { signupUser, loggingUser, signingOut };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,16 @@
-import express from 'express'
-import { signupUser } from '../controller/userController.js'
-
+import express from 'express';
+import { signupUser, loggingUser, signingOut } from '../controller/userController.js';
 
 const router = express.Router();
 
-//SIGN UP
+// SIGN UP
+router.post("/signup", signupUser);
 
-router.post("/signup",signupUser)
+// LOG IN
+router.post("/login", loggingUser);
+
+// SIGNING OUT
+router.post("/signout", signingOut);
+
 
 export default router;
