e396ee614227c20ec2fd114ad71278d32d83f323
SnehalRay <snehal1@ualberta.ca>
2024-07-12 03:02:28 +0400

    yoo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import { LogoutButton } from './components/LogoutButton';
 import { EditProfilePage } from './pages/EditProfilePage';
 import { CreatePost } from './components/CreatePost';
 import { FaHome } from "react-icons/fa";
+import { RiRobot2Fill } from "react-icons/ri";
+import Chatbot from './components/ChatBot';
 
 function App() {
   const user = useRecoilValue(userAtom);
@@ -23,6 +25,7 @@ function App() {
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/post/:pid" element={<PostPage />} />
         <Route path='/editProfile' element={user ? <EditProfilePage /> : <Navigate to="/authentication" />} />
+        <Route path='/chatBot' element={<Chatbot/>}/>
       </Routes>
 
       {user && (
@@ -32,9 +35,16 @@ function App() {
           right={["10px", "30px"]}
           alignItems="center"
         >
-          <Link to={`/${user.username}`}>
-            <Avatar name={user.name} src={user.profilePic} size={["sm", "md"]} mr={[3, 6]} cursor="pointer" bg="purple.500" color="white" />
+          <Link to="/chatBot">
+          <IconButton
+              icon={<RiRobot2Fill style={{ fontSize: '1.5em' }} />}
+              size={["sm", "md"]}
+              variant="ghost"
+              aria-label="ChatBot"
+              mr={[3, 6]}
+            />
           </Link>
+          
           <Link to="/">
             <IconButton
               icon={<FaHome style={{ fontSize: '1.5em' }} />}
@@ -43,6 +53,9 @@ function App() {
               aria-label="Home"
               mr={[3, 6]}
             />
+          </Link>
+          <Link to={`/${user.username}`}>
+            <Avatar name={user.name} src={user.profilePic} size={["sm", "md"]} mr={[3, 6]} cursor="pointer" bg="purple.500" color="white" />
           </Link>
           <Link to={"/"}>
           <LogoutButton size={["sm", "lg"]} />
