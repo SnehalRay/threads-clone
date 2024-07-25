c049da882def842b827383d1970c200a9204d99c
SnehalRay <snehal1@ualberta.ca>
2024-07-26 01:22:19 +0400

    Getting list of conversation API

--- backend/routes/messagesRoutes.js
+++ backend/routes/messagesRoutes.js
@@ -2,11 +2,13 @@ import express from 'express'
 import authenticateToken from '../middleware/authenticateToken.js';
 import sendMessage from '../controller/Messages/sendMessage.js';
 import getMessages from '../controller/Messages/getMessages.js';
+import getConversation from '../controller/Messages/getConversation.js';
 
 const messageRouter = express.Router();
 
 messageRouter.post("/sendMessage",authenticateToken,sendMessage);
 messageRouter.get("/getMessage/:otherUserId",authenticateToken,getMessages)
+messageRouter.get("/getConversation",authenticateToken,getConversation)
 
 
 
