af1e1119fbd9a23e2c757a3b8613a2ceb26b10be
SnehalRay <snehal1@ualberta.ca>
2024-07-30 22:03:47 +0400

    more vite config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://threads-clone-api-one.vercel.app',
-        changeOrigin: true,
-        secure: true
-      }
-    }
+  build: {
+    outDir: 'dist'
   }
-})
+});
