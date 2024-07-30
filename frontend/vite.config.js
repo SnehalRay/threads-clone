3ce0f91ecad169ff7a777fa7142d092c9f3cedea
SnehalRay <snehal1@ualberta.ca>
2024-07-30 21:34:41 +0400

    CORS config?

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    proxy:{
-      '/api':{
-        // target:'https://threads-clone-api-one.vercel.app/',
-        target:'http://localhost:8000/',
-        changeOrigin:true,
-        secure:false
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://threads-clone-api-one.vercel.app',
+        changeOrigin: true,
+        secure: true
       }
     }
   }
